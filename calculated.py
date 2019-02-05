7b31171db15cba0b11b97897f6ef57741d43dfe6
kowsalya13198 <37238527+kowsalya13198@users.noreply.github.com>
2019-02-05 11:07:27 +0530

    Create calculated.py

---
+++ calculated.py
@@ -0,0 +1,18 @@
+num1=int(input("enter the first num"))
+num2=int(input("enter the sec num"))   
+num3=int(input("enter the third num"))
+print("1.add")
+print("2.sub")
+print("3.mul")
+print("4.divide")
+choice=int(input("enter your choice"))
+if(choice==1):
+    print(num1+num2+num3)
+elif(choice==2):
+    print(num1-num2-num3)
+elif(choice==3):
+    print(num1*num2*num3)
+elif(choice==4):
+    print(num1/num2/num3)
+            
+                  
