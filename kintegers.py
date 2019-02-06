08e465a74a1e6c0343a80aee8f5396b476e4a28a
kowsalya13198 <37238527+kowsalya13198@users.noreply.github.com>
2019-02-06 15:48:51 +0530

    Create kintegers.py

---
+++ kintegers.py
@@ -0,0 +1,9 @@
+try:
+    n,k=map(int,input().split())
+    l=list(map(int,input().split()))
+    sum=0
+    for i in range(k):
+        sum+=l(k):
+    print(sum)
+except:
+    print("invalid integers")
